63a7a37c294598154fc425ea17ea402a9279a215
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-09 13:54:55 -0500

    Added html components and placeholders. Added date and time function. Added intialization function.

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ Link to Deployed Page:
 
 References:
 Obtained favicon images from: https://www.freeiconspng.com
+Bootstrap form obtained from: https://getbootstrap.com/docs/4.0/components/forms/
+Obtained moment JS from my 05 Homework: 
+
 
 Development Process (See commits for more details):
+Wireframed page + Pseudocode
+
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
-// add a changing favicon based on the type of weather
+// Creates date dispalyed at top of page
+function headerDate () {
+    var date = moment().format('dddd MMMM Do YYYY, h:mm a');
+    $('#date-and-time').text(date);
+}
 
-// Refresh weather on an interval
+// Refresh Header Date and Time on 60 second Interval
+var refresh = setInterval(function () {
+    // Refreshes header date
+    headerDate();
+    }, 60000);
+
+
 // Refresh the Time and Date. Get some autolocation?
-
+// API https://openweathermap.org/api/one-call-api
 
 // Be able to search for a city
 // City is added to a search history
+var searchHistroy = {};
+// Need to be saved locally
 // Cities in search history can be selected.
 // Once selected the same info can be viewed again.
-
+// Consider a delete button for search history
 // City result displays current and future conditions
 
 // Be able further extrapolate on current weather conditions from city to display:
@@ -20,3 +32,12 @@
 // Be able to further extrapolate on future weather conditions
 // Data includes a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 
+// Initiates base app
+function init () {
+    // Sets header date
+    headerDate();
+
+    // loads data from storage
+
+}
+init();
